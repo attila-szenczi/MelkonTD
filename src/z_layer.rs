d8e2104b497b76ed3ee2fbe12ac0de128bf60430
attila.szenczi <szenczi.attila90@gmail.com>
2020-05-27 23:30:18 +0200

    improve z layer

--- src/z_layer.rs
+++ src/z_layer.rs
@@ -1,11 +1,8 @@
 pub enum ZLayer {
-    TileMap,
+    TileMap = 0,
     Minion,
 }
 
 pub fn z_layer_to_coordinate(layer: ZLayer) -> f32 {
-    match layer {
-        ZLayer::TileMap => 0.,
-        ZLayer::Minion => 0.01,
-    }
+    layer as i32 as f32 / 100.
 }
